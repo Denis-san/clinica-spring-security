00af3872e04306e8c849cc00b740765417e6d51c
Denis-san <denilson.nt.nc@gmail.com>
2022-12-31 11:26:57 -0300

    remember-me config

--- src/main/java/com/mballem/curso/security/config/SecurityConfig.java
+++ src/main/java/com/mballem/curso/security/config/SecurityConfig.java
@@ -59,7 +59,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.logoutSuccessUrl("/")
 			.and()
 				.exceptionHandling()
-				.accessDeniedPage("/acesso-negado");
+				.accessDeniedPage("/acesso-negado")
+			.and()
+				.rememberMe();
 	}
 
 	@Override
